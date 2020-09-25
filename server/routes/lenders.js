847858fcbb80c4aa2bcf4a56cf3f350ca75c0ff2
Jorgen Carlsen <jscarlsen@gmail.com>
2020-09-25 14:17:37 -0700

    Setup and config jest and enzyme

--- server/routes/lenders.js
+++ server/routes/lenders.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const Lender = require('../../models/lender.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Getting lenders...');
+});
+
+router.get('/:id', (req, res) => {
+  // TODO..
+});
+
+router.post('/', (req, res) => {
+  // TODO..
+});
+
+router.put('/:id', (req, res) => {
+  // TODO..
+});
+
+router.delete('/:id', (req, res) => {
+  // TODO..
 });
 
 module.exports = router;
