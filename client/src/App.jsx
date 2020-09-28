625d2be36188d7aea2f45636e6fb0bead5375523
Jorgen Carlsen <jscarlsen@gmail.com>
2020-09-28 14:11:38 -0700

    Create and render Controls, Display and Header components. Write enzyme tests ensuring they exist.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from './components/Header.jsx';
+import Controls from './components/Controls.jsx';
+import Display from './components/Display.jsx';
 
-const App = () => (<div>Hello World!</div>);
+const AppContainer = styled.div`
+  width: 100%;
+  padding: 20px;
+`;
+
+const App = () => (
+  <AppContainer>
+    <Header />
+    <Controls />
+    <Display />
+  </AppContainer>
+);
 
 export default App;
