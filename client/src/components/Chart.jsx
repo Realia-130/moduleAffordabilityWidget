9cc436295e37902ef8c86e79f1b0abb722a91aa1
Jorgen Carlsen <jscarlsen@gmail.com>
2020-09-29 10:45:26 -0700

    Implement header, start styling sliders on controls

---
+++ client/src/components/Chart.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Chart = () => (<div>Chart</div>);
+
+export default Chart;
--- client/src/components/HomePrice.jsx
+++ client/src/components/HomePrice.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import RangeSlider from './RangeSlider.jsx';
 
 const HomePriceContainer = styled.div`
   flex: 1;
@@ -28,18 +29,31 @@ const TopContainer = styled.div`
 
 const HomePrice = ({ homePrice }) => {
   const [value, setValue] = useState(homePrice);
+  const [fill, setFill] = useState(75);
+  const max = homePrice * 1.5;
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setFill((e.target.value / max) * 100);
+  };
 
-  const handleChange = (e) => setValue(e.target.value);
+  const styles = {
+    background: `linear-gradient(to right,
+      rgb(0, 120, 130) 0%,
+      rgb(0, 120, 130) ${fill}%,
+      rgb(205, 209, 212) ${fill}%,
+      rgb(205, 209, 212) 100%)`,
+  };
 
   return (
     <HomePriceContainer>
-      <TopContainer>
+      <TopContainer className="top-container">
         <h4>Home Price</h4>
         <input type="text" className="money-input" />
       </TopContainer>
-      <input className="range" type="range" min="0" max="1500000" step="10" value={value} onChange={handleChange} />
+      <input style={styles} className="range" type="range" min="0" max={max} step="10" value={value} onChange={handleChange} />
+      {/* <RangeSlider min={0} max={1500000} /> */}
     </HomePriceContainer>
   );
-}
+};
 
 export default HomePrice;
